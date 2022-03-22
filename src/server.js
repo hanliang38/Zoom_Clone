c1625bb6e47dd1f41a3c925c2b4877f1272c4082
Mira Kwak <mira@Miraui-MacBookPro.local>
2022-03-22 21:51:02 +0900

    2022-03-22|feat(chat):Chat completed

--- src/server.js
+++ src/server.js
@@ -18,11 +18,15 @@ const handleListen = () => console.log(`Listening on http://localhost:${port}`)
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
+const sockets = []
+
 wss.on("connection", (socket) => {
+  sockets.push(socket)
   console.log("Connected to Browser ✅")
   socket.on("close", () => console.log("Disconnected from Browser ❌"))
-  socket.on("message", message => console.log(message.toString('utf8')))
-  socket.send("Hello!!!")
+  socket.on("message", (message) => {
+    sockets.forEach(aSocket => aSocket.send(message.toString('utf8')))
+  })
 })
 
 server.listen(port, handleListen);
