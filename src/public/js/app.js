d4e14cb43c3eaf3d2cb23589f56a366ce8b37f99
Mira Kwak <mira@Miraui-MacBookPro.local>
2022-03-25 06:07:00 +0900

    2022-03-25|feat(room):nickname,room count,user count

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -18,11 +18,18 @@ function addMessage(message) {
 
 function handleMessageSubmit(e) {
   e.preventDefault()
-  const input = room.querySelector("input")
+  const input = room.querySelector("#msg input")
   const value = input.value
   socket.emit("new_message",input.value, roomName, () => {
     addMessage(`You:${value}`)
   })
+}
+
+function handleNicknameSubmit(e) {
+  e.preventDefault()
+  const input = room.querySelector("#name input")
+  const value = input.value
+  socket.emit("nickname",input.value)
 }
 
 function showRoom() {
@@ -30,8 +37,10 @@ function showRoom() {
   room.hidden = false
   const h3 = room.querySelector("h3")
   h3.innerText = `Room ${roomName}`
-  const form = room.querySelector("form")
-  form.addEventListener("submit", handleMessageSubmit)
+  const msgForm = room.querySelector("#msg")
+  const nameForm = room.querySelector("#name")
+  msgForm.addEventListener("submit", handleMessageSubmit)
+  nameForm.addEventListener("submit", handleNicknameSubmit)
 }
 
 function handleRoomSubmit(e) {
@@ -44,12 +53,29 @@ function handleRoomSubmit(e) {
 
 form.addEventListener("submit",handleRoomSubmit)
 
-socket.on("welcome", () => {
-  addMessage(`Someone joined!`)
+socket.on("welcome", (user, newCount) => {
+  const h3 = room.querySelector("h3")
+  h3.innerText = `Room ${roomName}(${newCount})`
+  addMessage(`${user} joined!`)
 })
 
-socket.on("bye", () => {
-  addMessage(`Someone left! ㅠ_ㅠ`)
+socket.on("bye", (left, newCount) => {
+  const h3 = room.querySelector("h3")
+  h3.innerText = `Room ${roomName}(${newCount})`
+  addMessage(`${left} left! ㅠ_ㅠ`)
 })
 
 socket.on("new_message", addMessage)
+
+socket.on("room_change", rooms => {
+  const roomList = welcome.querySelector("ul")
+  roomList.innerHTML = ""
+  if(rooms.length === 0){
+    return
+  }
+  rooms.forEach(room => {
+    const li = document.createElement("li")
+    li.innerText = room
+    roomList.append(li)
+  });
+})
