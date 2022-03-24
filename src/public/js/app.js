242a1a5ea47e3edd26850e5375d8149856134d5c
Mira Kwak <mira@Miraui-MacBookPro.local>
2022-03-25 04:22:46 +0900

    2022-03-25|feat(room):enter room message other

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,12 +2,33 @@ const socket = io()
 
 const welcome = document.getElementById("welcome")
 const form = welcome.querySelector("form")
+const room = document.getElementById("room")
+
+
+room.hidden = true
+
+let roomName;
+
+function addMessage(message){
+  const ul = room.querySelector("ul")
+  const li = document.createElement("li")
+  li.innerText = message
+  ul.appendChild(li)
+}
 
 form.addEventListener("submit", (e)=>{
   e.preventDefault()
   const input = form.querySelector("input")
-  socket.emit("enter_room",{payload: input.value},()=>{
-    console.log("server is done!")
+  socket.emit("enter_room",input.value,()=>{
+    welcome.hidden = true
+    room.hidden = false
+    const h3 = room.querySelector("h3")
+    h3.innerText = `Room ${roomName}`
   })
+  roomName = input.value
   input.value=""
+})
+
+socket.on("welcome", () => {
+  addMessage(`Someone joined!`)
 })
--- src/server.js
+++ src/server.js
@@ -17,36 +17,18 @@ const handleListen = () => console.log(`Listening on http://localhost:${port}`)
 // app.listen(port, handleListen);
 
 // http 서버 위에 webSocket 서버를 만을 수 있도록 함 (http 서버에 access)
-const httpServer = http.createServer(app);
+const httpServer = http.createServer(app)
 const wsServer = SocketIO(httpServer)
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room",(msg,done)=>{
-    console.log(msg)
-    setTimeout(() =>{
-      done()
-    }, 10000)
+  socket.onAny((e) => {
+    console.log(`Sockent Event:${e}`)
+  })
+  socket.on("enter_room",(roomName, done)=>{
+    socket.join(roomName)
+    done()
+    socket.to(roomName).emit("welcome")
   })
 })
-
-// const wss = new WebSocket.Server({server});
-
-// const sockets = []
-// wss.on("connection", (socket) => {
-//   sockets.push(socket)
-//   socket["nickname"] = "Anon"
-//   console.log("Connected to Browser ✅")
-//   socket.on("close", () => console.log("Disconnected from Browser ❌"))
-//   socket.on("message", (msg) => {
-//     const message = JSON.parse(msg)
-//     // if else 대신에 switch
-//     switch (message.type) {
-//       case "new_message":
-//         sockets.forEach(aSocket => aSocket.send(`${socket.nickname}:${message.payload}`))
-//       case "nickname":
-//         socket["nickname"] = message.payload
-//     }
-//   })
-// })
 
 httpServer.listen(port, handleListen);
