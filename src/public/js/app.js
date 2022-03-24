f81aee4b36fec5a1c8d17a0a975aeec0ed8624b2
Mira Kwak <mira@Miraui-MacBookPro.local>
2022-03-25 07:49:26 +0900

    2022-03-25|error(camera):Video screen not working

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,81 +1,45 @@
-const socket = io()
+const socket = io();
 
-const welcome = document.getElementById("welcome")
-const form = welcome.querySelector("form")
-const room = document.getElementById("room")
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-room.hidden = true
-
-let roomName;
-
-function addMessage(message) {
-  const ul = room.querySelector("ul")
-  const li = document.createElement("li")
-  li.innerText = message
-  ul.appendChild(li)
-}
-
-function handleMessageSubmit(e) {
-  e.preventDefault()
-  const input = room.querySelector("#msg input")
-  const value = input.value
-  socket.emit("new_message",input.value, roomName, () => {
-    addMessage(`You:${value}`)
-  })
-}
-
-function handleNicknameSubmit(e) {
-  e.preventDefault()
-  const input = room.querySelector("#name input")
-  const value = input.value
-  socket.emit("nickname",input.value)
-}
-
-function showRoom() {
-  welcome.hidden = true
-  room.hidden = false
-  const h3 = room.querySelector("h3")
-  h3.innerText = `Room ${roomName}`
-  const msgForm = room.querySelector("#msg")
-  const nameForm = room.querySelector("#name")
-  msgForm.addEventListener("submit", handleMessageSubmit)
-  nameForm.addEventListener("submit", handleNicknameSubmit)
-}
-
-function handleRoomSubmit(e) {
-  e.preventDefault()
-  const input = form.querySelector("input")
-  socket.emit("enter_room",input.value, showRoom)
-  roomName = input.value
-  input.value=""
-}
-
-form.addEventListener("submit",handleRoomSubmit)
-
-socket.on("welcome", (user, newCount) => {
-  const h3 = room.querySelector("h3")
-  h3.innerText = `Room ${roomName}(${newCount})`
-  addMessage(`${user} joined!`)
-})
-
-socket.on("bye", (left, newCount) => {
-  const h3 = room.querySelector("h3")
-  h3.innerText = `Room ${roomName}(${newCount})`
-  addMessage(`${left} left! ㅠ_ㅠ`)
-})
-
-socket.on("new_message", addMessage)
-
-socket.on("room_change", rooms => {
-  const roomList = welcome.querySelector("ul")
-  roomList.innerHTML = ""
-  if(rooms.length === 0){
-    return
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
   }
-  rooms.forEach(room => {
-    const li = document.createElement("li")
-    li.innerText = room
-    roomList.append(li)
-  });
-})
+}
+
+getMedia();
+
+function handleMuteClick() {
+  if (!muted) {
+    muteBtn.innerText = "소리 켜기";
+    muted = true;
+  } else {
+    muteBtn.innerText = "소리 끄기";
+    muted = false;
+  }
+}
+function handleCameraClick() {
+  if (cameraOff) {
+    cameraBtn.innerText = "카메라 끄기";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "카메라 켜기";
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
