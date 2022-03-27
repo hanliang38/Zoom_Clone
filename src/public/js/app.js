1ff1c832b0dda13d0a9b0423a9b74f9e3f2058dd
Mira Kwak <mira@Miraui-MacBookPro.local>
2022-03-28 01:53:03 +0900

    2022-03-28|feat(rooms):create video call rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,22 +4,26 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const cameraSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
+
+call.hidden = true;
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName
 
 async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
-    const currentCamera = myStream.getVideoTracks()[0]
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
       if (currentCamera.label === camera.label) {
-        option.selected = true
+        option.selected = true;
       }
       cameraSelect.appendChild(option);
     });
@@ -43,13 +47,12 @@ async function getMedia(deviceId) {
     );
     myFace.srcObject = myStream;
     if (!deviceId) {
-      await getCameras()
+      await getCameras();
     }
   } catch (e) {
     console.log(e);
   }
 }
-getMedia();
 
 function handleMuteClick() {
   myStream
@@ -83,3 +86,30 @@ async function handleCameraChange() {
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 cameraSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+function startMedia() {
+  welcome.hidden = true;
+  call.hidden = false;
+  getMedia();
+}
+
+function handleWelcomeSubmit(e) {
+  e.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  socket.emit("join_room", input.value, startMedia);
+  roomName = input.value
+  input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+
+//socket code
+
+socket.on("welcome", () => {
+  console.log("someone joined")
+})
--- src/server.js
+++ src/server.js
@@ -22,5 +22,13 @@ instrument(wsServer, {
   auth: false,
 });
 
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+});
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
